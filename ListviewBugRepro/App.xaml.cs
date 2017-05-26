4e19989a0f4425656d871e3a204468f3d4fd7638
fschwiet <fschwiet@gmail.com>
2017-05-26 12:34:26 -0500

    Changes originally staged after creating project

---
+++ ListviewBugRepro/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+
+namespace ListviewBugRepro
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new ListviewBugReproPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ ListviewBugRepro/ListviewBugReproPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ListviewBugRepro
+{
+    public partial class ListviewBugReproPage : ContentPage
+    {
+        public ListviewBugReproPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
